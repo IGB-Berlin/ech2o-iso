af7ae33fde38645f1af27bff274beb1322282e71
Ech2o Tracer <ech2o.tracer@igb-berlin.de>
2021-06-10 13:26:53 +0200

    Corrected nonlinear equation Line not commented out

--- Forest/SolveCanopyEnergyBalance.cpp
+++ Forest/SolveCanopyEnergyBalance.cpp
@@ -234,8 +234,8 @@ UINT4 Forest::SolveCanopyEnergyBalance(Basin &bas, Atmosphere &atm, Control &ctr
 	lambda = x[2] < 0 ? lat_heat_vap + lat_heat_fus : lat_heat_vap;
 
 	//gc = dgcdfgspsi*std::max<REAL8>(0,std::min<REAL8>(1,(lwp_min - x[1]) /
-							  (lwp_min - lwp_max)));
-	gc = dgcdfgspsi * 1 / (1 + powl(x[1]/lwp_den, lwp_c));
+	//						  (lwp_min - lwp_max)));
+	gc = dgcdfgspsi * 1 / (1 + powl(x[1]/lwp_min, lwp_max));
 
 	if (gc < 1e-13)
 	  gc = 1e-13;
