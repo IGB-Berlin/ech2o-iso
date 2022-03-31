0164edc24af8586dce355e728d4c3001413f648c
Ech2o Tracer <ech2o.tracer@igb-berlin.de>
2022-03-31 15:59:04 +0200

    Remove the library call to netcdf

--- Release-Linux/objects.mk
+++ Release-Linux/objects.mk
@@ -4,6 +4,6 @@
 
 USER_OBJS :=
 
-LIBS := -lcsf -lboost_serialization -larmadillo -lnetcdf-cxx4 #-llapack -lblas
+LIBS := -lcsf -lboost_serialization -larmadillo #-lnetcdf-cxx4 #-llapack -lblas
 
 # only uncomment lapack and blas calls if armadillo prompts linker errors (arma:: etc.) 
